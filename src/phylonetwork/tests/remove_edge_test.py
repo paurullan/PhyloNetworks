d9b01582dadb5b2540210c2937c759eba926540d
Pau Ruŀlan Ferragut <paurullan@gmail.com>
2013-04-30 14:48:57 +0200

    Beginning of tests

---
+++ src/phylonetwork/tests/remove_edge_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from ..classes import PhyloNetwork
+
+class TestPhyloNet(unittest.TestCase):
+
+    def test_edge_removable(self):
+        pass
+
+    def test_detect_edges(self):
+        """Find the four removable edges"""
+        pass
